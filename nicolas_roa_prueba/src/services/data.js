798ce1a97a708be5781f6392ff614bc98bd4db43
NroaPrado <74987069+NroaPrado@users.noreply.github.com>
2022-01-31 12:07:34 -0300

    Data.js
    aqui es donde estaremos consumiendo nuestro url con JSON

---
+++ nicolas_roa_prueba/src/services/data.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react'
+
+export const useFetch = () => {
+
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+
+    setLoading(true)
+    setError(null)
+
+    fetch('https://data.cdc.gov/api/views/bxq8-mugm/rows.json')
+      .then(res => res.json())
+      .then(json => {
+        setLoading(false)
+        if (json) {
+          setData(json)
+        } else {
+          setData([])
+        }
+      })
+      .catch(err => {
+        setError(err)
+        setLoading(false)
+      })
+  }, [])
+  return { data, loading, error 
+}
